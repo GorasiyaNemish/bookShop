a3ceada8654355db244c9af57d56e8d41190f176
GorasiyaNemish <nemishgorasiya@gmail.com>
2023-08-05 17:04:52 +0530

    done signup

--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from 'formik';
+import {Form, Formik ,formik} from 'formik';
 import React from 'react';
 import { Grid, Button, TextField } from '@mui/material';
 
@@ -15,26 +15,29 @@ const Signup = () => {
           password: ""
         }}
         onSubmit={(values) => {
-          alert(values.firstName + "\nfcvgbhjn");
+          alert(JSON.stringify(values, null, 2));
         }}
       >
-        <Form style={{ padding: "80px 20%", display: "flex", flexDirection: "column", width: "100%" }}>
+        {({values,handleChange})=>(
+          <Form style={{ padding: "80px 20%", display: "flex", flexDirection: "column", width: "100%" }}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              <TextField name="firstName" type="text" label="First Name" fullWidth />
+              <TextField name="firstName" type="text" label="First Name" fullWidth value={values.firstName} onChange={handleChange}/>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField name="lastName" type="text" label="Last Name" fullWidth />
+              <TextField name="lastName" type="text" label="Last Name" fullWidth value={values.lastName} onChange={handleChange} />
             </Grid>
             <Grid item sm={12}>
-              <TextField name="email" type="text" label="Email" fullWidth />
+              <TextField name="email" type="text" label="Email" fullWidth value={values.email} onChange={handleChange} />
             </Grid>
             <Grid item sm={12}>
-              <TextField name="password" type="password" label="Password" fullWidth />
+              <TextField name="password" type="password" label="Password" fullWidth value={values.password} onChange={handleChange} />
             </Grid>
           </Grid>
           <Button type="submit" color="primary" variant='contained' sx={{my:3}}>Submit</Button>
         </Form>
+        )}
+        
       </Formik>
       </div>
     </>
